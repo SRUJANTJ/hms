704cc342152365176b6c367ce2f9bc85900616d6
SRUJANTJ <91385622+SRUJANTJ@users.noreply.github.com>
2026-08-18 13:50:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,20 @@ export default function Home() {
       .then((r) => (r.ok ? r.json() : Promise.reject()))
       .then((data) => {
         const role = data.user.role;
-        if (role === "admin") router.replace("/admin");
-        else if (role === "warden") router.replace("/warden");
-        else router.replace("/student");
+
+        if (role === "admin") {
+          router.replace("/admin");
+        } else if (role === "warden") {
+          router.replace("/warden");
+        }
+/* else {
+          router.replace("/student");
+        }*\
       })
-      .catch(() => router.replace("/warden/login"));
-  }, []);
+      .catch(() => {
+        router.replace("/warden/login");
+      });
+  }, [router]);
 
   return (
     <div className="min-h-screen flex items-center justify-center text-gray-400">
